5d90365f21c2bbe5517473550659e41c0e7dd694
adumbidiot <nathaniel.daniel23@outlook.com>
2017-11-26 17:36:18 -0800

    RiotJs/BlazeCss initial experiment

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fastify = require('fastify')();
 const fastifyStatic = require('fastify-static');
 const path = require('path');
+const PORT = 8080;
 
 fastify.register(fastifyStatic, {
 	root: path.join(__dirname, 'public')
 });
 
-fastify.listen(8080, function(err){
+fastify.listen(PORT, function(err){
 	if(err){
 		throw err;
 	}else{
-		console.log('server running');
+		console.log('Server Running on port ' + PORT);
 	}
 });
---
+++ public/tags/navbar.tag
@@ -0,0 +1,11 @@
+<navbar>
+	<ul class="c-nav c-nav--inline">
+		<li class="c-nav__item">
+			<a href="/" class="o-page-header__button o-page-header__button--left c-link c-link--brand" style="color: inherit"><i class="fa fa-home"></i>Home</a>
+		</li>
+	
+		<!--<li class="c-nav__item"><i class="fa fa-star"></i> News</li>
+		<li class="c-nav__item c-nav__item--right"><i class="fa fa-user"></i> Contact</li>-->
+	
+	</ul>
+</navbar>
