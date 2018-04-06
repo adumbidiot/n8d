ae6dc92e5bf2631a1f5042cf166a19886de9100e
adumbidiot <nathaniel.daniel23@outlook.com>
2018-04-05 21:56:16 -0700

    Finished Fibonacci Sequence Visual Class
    Finished Visual class for ease of use and cleaned up code. Added https
    support. Removed extraneous png file.

---
+++ public/experiments/visual-fibonacci/js/fibonacci-visual.js
@@ -0,0 +1,59 @@
+class fibonacciVisual{
+	constructor(opts){
+		opts = opts || {};
+		if(opts.canvasID){
+			this.canvas = document.getElementById(opts.canvasID);
+		}
+		this.canvas = this.canvas || opts.canvas || document.createElement('canvas');
+		this.nFunc = opts.nFunc || (function(){
+			return 2;
+		});
+		this.ctx = this.canvas.getContext('2d');
+		this.drawData = [];
+		this.fibonacci = new fibonacciVisualCore();
+	}
+	setN(n){
+			this.nFunc = function(){
+				return (function(){
+					return n;
+				})();
+			};
+	}
+	setNFunc(func){
+		this.nFunc = func;
+	}
+	get n(){
+		return this.nFunc();
+	}
+	draw(){
+		this.resize().clear();
+		this.ctx.fillStyle = "black";
+		for(let  i = 0; i != this.drawData.length; i++){
+			let data = this.drawData[i].toArray(2).value;
+			for(let j = 0; j != data.length; j++){
+				if(data[data.length - 1 - j] === 1){
+					this.ctx.fillRect(j * 10,i * 10, 10, 10);
+				}		
+			}
+		}
+		return this;
+	}
+	clear(){
+		this.ctx.fillStyle = 'white';
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		return this;
+	}
+	calc(){
+		this.drawData = this.fibonacci.calc(this.n).getSequence();
+		return this;
+	}
+	resize(){
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		canvas.height = this.n * 10;
+		canvas.width = this.n * 10;
+		return this;
+	}
+	produceImage(){
+		this.calc().draw();
+	}
+}
