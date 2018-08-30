28f87703b84ccfc56f64fdd35cc1fdca773cd77c
Nathaniel <nathaniel.daniel23@outlook.com>
2018-08-30 14:41:40 -0700

    began to work on ad replacement for boyce block

--- public/experiments/boyce-block/main.js
+++ public/experiments/boyce-block/main.js
@@ -1,29 +1,89 @@
 console.log("hi");
 
 let hostTriggers = [
-	"ads.pictela.net"
+	"ads.pictela.net",
+	"rcm-na.amazon-adsystem.com",
+	"googleads.g.doubleclick.net",
+	"tpc.googlesyndication.com",
+	"s0.2mdn.net"
+];
+
+let titleTriggers = [
+	"Ad"
+];
+
+let classTriggers = [
+	"outeradcontainer",
+	"adsbygoogle"
 ];
 
 function replace(el){
 	let img = new Image();
-	img.src = 'images/jacob128.png';
+	img.src = chrome.extension.getURL('/images/jacob128.png');
 	img.style.cssText = el.style.cssText;
+	if(el.width){
+		img.width = el.width;
+	}
+	
+	if(el.height){
+		img.height = el.height;
+	}
 	
 	el.style.display = "none";
-	console.log(el.parentNode, el, el.parentNode.children[0]);
-	el.parentNode.insertBefore(el.parentNode.children[0], img);
+	el.before(img);
 }
 
 
-window.addEventListener('load', function(){
+window.addEventListener('load', removeAds);
+
+function removeAds(){
 	let iframes = document.getElementsByTagName("iframe");
+	console.log(iframes);
 	
 	for(let i = 0; i != iframes.length; i++){
-		console.log(iframes[i]);
-		let url = new URL(iframes[i].src);
-		if(hostTriggers.includes(url.host)){
-			console.log("bad");
-			replace(iframes[i]);
-		} 
+		if(iframes[i].src){
+			let url = new URL(iframes[i].src);
+			console.log(url.host);
+			if(hostTriggers.includes(url.host)){
+				console.log("bad host", iframes[i]);
+				replace(iframes[i]);
+			} 
+		}
+		
+		if(iframes[i].title){
+			if(titleTriggers.includes(iframes[i].title)){
+				console.log("Bad title");
+				replace(iframes[i]);
+			}
+		}
+		
+		if(iframes[i].class){
+			if(classTriggers.includes(iframes[i].class)){
+				console.log("Bad title");
+				replace(iframes[i]);
+			}
+		}
+		
+		//setTimeout(removeAds, 5000);
 	}
-});
+	let ins = document.getElementsByTagName("ins");
+	console.log(ins);
+	for(let i = 0; i != ins.length; i++){
+		console.log(ins[i].className);
+		if(ins[i].className && scanElementbyClass(ins[i], classTriggers)){
+			console.log("bad class");
+			replace(ins[i]);
+		}
+	}
+	
+	setTimeout(removeAds, 5000);
+}
+
+function scanElementbyClass(el, blacklist){
+	for(let i = 0; i != el.classList.length; i++){
+		if(blacklist.includes(el.classList[i])){
+			return true;
+		}
+	}
+	return false;
+}
